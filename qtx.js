1484d7eb2fea1f72a637b8ce299d088c4479e825
smallfawn <101914820+smallfawn@users.noreply.github.com>
2023-07-06 16:16:27 +0800

    Update qtx.js

--- qtx.js
+++ qtx.js
@@ -300,29 +300,27 @@ function httpRequest(options, method) {
         })
     })
 }
-async function notice() {
+/**
+ * 获取远程通知
+ */
+async function getNotice() {
     try {
-        let options = {
-            url: `https://ghproxy.com/https://raw.githubusercontent.com/smallfawn/api/main/notice.json`,
-            headers: {
-                'User-Agent': ''
-            },
-        }
-        //console.log(options);
-        let result = await httpRequest(options);
-        //console.log(result);
-        if (result) {
-            if ('notice' in result) {
-                DoubleLog(`${result.notice}`);
-            } else {
-                options.url = `https://gitee.com/smallfawn/api/raw/master/notice.json`
-                result = await httpRequest(options);
-                if ('notice' in result) {
-                    DoubleLog(`${result.notice}`);
-                }
+        const urls = [
+            "https://cdn.jsdelivr.net/gh/smallfawn/Note@main/Notice.json",
+            "https://ghproxy.com/https://raw.githubusercontent.com/smallfawn/Note/main/Notice.json",
+            "https://fastly.jsdelivr.net/gh/smallfawn/Note@main/Notice.json",
+            "https://gitee.com/smallfawn/Note/raw/master/Notice.json",
+        ];
+        let notice = null;
+        for (const url of urls) {
+            const options = { url, headers: { "User-Agent": "" }, };
+            const result = await httpRequest(options);
+            if (result && "notice" in result) {
+                notice = result.notice.replace(/\\n/g, "\n");
+                break;
             }
-        } else {
         }
+        if (notice) { $.DoubleLog(notice); }
     } catch (e) {
         console.log(e);
     }
