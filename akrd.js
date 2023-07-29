4417e7bccbbb526296ea5c6aad8c339387658fc9
smallfawn <101914820+smallfawn@users.noreply.github.com>
2023-07-29 18:58:00 +0800

    爱看热点：修改注释

--- akrd.js
+++ akrd.js
@@ -3,8 +3,8 @@
  * cron 11 9,18 * * *  akrd.js
  * Show: 每天1
  * 变量名:akrd
- * 变量值:https://apiakrd.cengaw.cn/api headers中device的值&Authorization的值（去掉Bearer）&User-Agent中（）里面的值 例如(Linux; U; Android 11; XIAOMI 8 MIUI/V11.0.1.0.QXXXXXM)
- * 完整格式 3xxxxxxxxxxxxxxa&eyJ0eXAiOiJKV1M...&(Linux; U; Android 11; XIAOMI 8 MIUI/V11.0.1.0.QXXXXXM)
+ * 变量值:https://apiakrd.cengaw.cn/api headers中Authorization的值（去掉Bearer）&device的值&User-Agent中（）里面的值 例如(Linux; U; Android 11; XIAOMI 8 MIUI/V11.0.1.0.QXXXXXM)
+ * 完整格式 eyJ0eXAiOiJKV1M...&3xxxxxxxxxxxxxxa&(Linux; U; Android 11; XIAOMI 8 MIUI/V11.0.1.0.QXXXXXM)
  * 缺一不可 黑号自负
  * 多账号 @ 分割
  * scriptVersionNow = "0.0.1";
