584944eb6d0b25619fe06b4329c9548e7f5fffcc
smallfawn <101914820+smallfawn@users.noreply.github.com>
2023-11-03 14:57:27 +0800

    Update suboer.js

--- suboer.js
+++ suboer.js
@@ -22,8 +22,8 @@ let msg = "";
 
 
 async function start() {
-    await getVersion("smallfawn/QLScriptPublic/main/suboer.js");
-    await getNotice();
+    //await getVersion("smallfawn/QLScriptPublic/main/suboer.js");
+    //await getNotice();
 
     let taskall = [];
     for (let user of $.userList) {
