b4d4eaffac166a4cd833d4274da0db1ad5124386
smallfawn <860562056@QQ.COM>
2026-04-02 20:06:59 +0800

    feat(weilai): new weilai.js

--- daily/yingsheng.js
+++ daily/yingsheng.js
@@ -16,7 +16,7 @@ cron: 12 12 * * *
 6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
 7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
 */
-const { Env } = require("./tools/env")
+const { Env } = require("../tools/env")
 const $ = new Env("硬声");
 let ckName = `yingsheng`;
 const strSplitor = "#";
