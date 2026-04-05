c38936c5c9920f910f199f673251d9514883509c
smallfawn <860562056@QQ.COM>
2026-04-05 16:40:14 +0800

    fix bug

--- daily/mxbc.js
+++ daily/mxbc.js
@@ -19,7 +19,7 @@ cron: 30 8 * * *
 7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
 */
 
-const { Env } = require("./tools/env")
+const { Env } = require("../tools/env")
 const $ = new Env("蜜雪冰城");
 let ckName = `mxbc`;
 const strSplitor = "#";
